7d4ecb08861cc2b372cceef7ee24068c4f043545
Terry Tan <tmy1995hflc@gmail.com>
2019-01-18 19:56:00 -0600

    q5 q138

--- Hash Table/Q138.py
+++ Hash Table/Q138.py
@@ -1,9 +1,8 @@
-# Definition for singly-linked list with a random pointer.
-# class RandomListNode(object):
-#     def __init__(self, x):
-#         self.label = x
-#         self.next = None
-#         self.random = None
+class RandomListNode(object):
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
 
 class Solution(object):
     def copyRandomList(self, head):
@@ -11,4 +10,17 @@ class Solution(object):
         :type head: RandomListNode
         :rtype: RandomListNode
         """
-        
+        nodeDict = dict()
+        dummy = RandomListNode(0)
+        pointer, newHead = head, dummy
+        while pointer:
+            newNode = RandomListNode(pointer.label)
+            nodeDict[pointer] = newNode
+            newHead.next = newNode
+            pointer, newHead = pointer.next, newHead.next
+        pointer, newHead = head, dummy.next
+        while pointer:
+            if pointer.random:
+                newHead.random = nodeDict[pointer.random]
+            pointer, newHead = pointer.next, newHead.next
+        return dummy.next
--- String/Q5.py
+++ String/Q5.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        def palindrome(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
         
+        answer = ""
+
+        for i in range(len(s)):
+            str1 = palindrome(s, i, i)
+            if len(str1) > len(answer): answer = str1
+            str2 = palindrome(s, i, i + 1)
+            if len(str2) > len(answer): answer = str2
+        
+        return answer
+sol = Solution()
+print(sol.longestPalindrome("cbbd"))
