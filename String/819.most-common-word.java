e5e1b3c2b13cdb4eed2ef682430978f8a4436d79
Terry Tan <tmy1995hflc@gmail.com>
2019-02-19 01:13:57 -0600

    819

--- String/819.most-common-word.java
+++ String/819.most-common-word.java
@@ -56,12 +56,27 @@
  * 
  * 
  */
+import java.util.*;
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        paragraph = paragraph.replaceAll("[,.]", "");
-        String[] list = paragraph.split(" ");
-        for (String item: list){
-            
+        Set<String> banSet = new HashSet<>(Arrays.asList(banned));
+        HashMap<String, Integer> stat = new HashMap<>();
+        String[] afterProcess = paragraph.replaceAll("[!?',;.]", " ").toLowerCase().trim().split("\\s+");
+        for(String item: afterProcess){
+            if(!banSet.contains(item)){
+                stat.put(item, stat.getOrDefault(item, 0) + 1);
+            }
         }
+        String result = "";
+        int max = -1;
+        int value = 0;
+        for(String key: stat.keySet()){
+            value = stat.get(key);
+            if(value > max){
+                max = value;
+                result = key;
+            }
+        }
+        return result;
     }
 }
