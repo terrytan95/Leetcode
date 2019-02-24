519ce063b73fe22415942ddaf22e72c53fe61af6
Terry Tan <tmy1995hflc@gmail.com>
2019-02-24 02:17:45 -0600

    139 bfs

---
+++ Unfinished/301.remove-invalid-parentheses.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=301 lang=java
+ *
+ * [301] Remove Invalid Parentheses
+ *
+ * https://leetcode.com/problems/remove-invalid-parentheses/description/
+ *
+ * algorithms
+ * Hard (38.17%)
+ * Total Accepted:    108.2K
+ * Total Submissions: 282.5K
+ * Testcase Example:  '"()())()"'
+ *
+ * Remove the minimum number of invalid parentheses in order to make the input
+ * string valid. Return all possible results.
+ * 
+ * Note: The input string may contain letters other than the parentheses ( and
+ * ).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()())()"
+ * Output: ["()()()", "(())()"]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "(a)())()"
+ * Output: ["(a)()()", "(a())()"]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: ")("
+ * Output: [""]
+ * 
+ */
+import java.util.*;
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        Stack<String> result = new Stack<>();
+        for()
+    }
+}
