e4c3f71a28915c69b7989c091e3c6644d73b3144
Terry Tan <tmy1995hflc@gmail.com>
2019-02-26 01:54:01 -0600

    811

--- Test.java
+++ Test.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 public class Test {
     public static void main(String args[]) {
-        int i = 0;
-        int j = i + 2;
-        i++;
-        i++;
-        System.out.println(j);
+        int[] i = {1,2,3,4,5};
+        System.out.println(i[i.length - 1]);
     }
 
 }
