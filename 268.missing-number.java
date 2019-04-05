0675a1bc0be953d0d111316f3addfb827b6d1ae9
Terry Tan <tmy1995hflc@gmail.com>
2019-04-05 11:59:55 -0500

    700

---
+++ 268.missing-number.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=268 lang=java
+ *
+ * [268] Missing Number
+ *
+ * https://leetcode.com/problems/missing-number/description/
+ *
+ * algorithms
+ * Easy (47.83%)
+ * Total Accepted:    259.1K
+ * Total Submissions: 541.2K
+ * Testcase Example:  '[3,0,1]'
+ *
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n,
+ * find the one that is missing from the array.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [3,0,1]
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * 
+ * 
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement
+ * it using only constant extra space complexity?
+ */
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+    }
+}
+
