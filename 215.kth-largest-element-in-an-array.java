f81c8d46d8e2896515541290c3a0c3953d910edc
Terry Tan <tmy1995hflc@gmail.com>
2019-04-05 11:46:00 -0500

    optimized 215

--- 215.kth-largest-element-in-an-array.java
+++ 215.kth-largest-element-in-an-array.java
@@ -34,43 +34,57 @@ import java.util.*;
  * 
  */
 class Solution {
-    public int findKthLargest(int[] a, int k) {
-        int n = a.length;
-        int p = quickSelect(a, 0, n - 1, n - k + 1);
-        return a[p];
+    public int findKthLargest(int[] nums, int k) {
+        return select(nums, k - 1);
     }
 
-    // return the index of the kth smallest number
-    public int quickSelect(int[] a, int lo, int hi, int k) {
-        // use quick sort's idea
-        // put nums that are <= pivot to the left
-        // put nums that are > pivot to the right
-        int i = lo, j = hi, pivot = a[hi];
-        while (i < j) {
-            if (a[i++] > pivot) {
-                swap(a, --i, --j);
+    // Quick select
+    private int select(int[] nums, int k) {
+        int left = 0, right = nums.length - 1;
+        while (true) {
+            if (left == right)
+                return nums[left];
+            int pivotIndex = medianOf3(nums, left, right);
+            pivotIndex = partition(nums, left, right, pivotIndex);
+            if (pivotIndex == k)
+                return nums[k];
+            else if (pivotIndex > k)
+                right = pivotIndex - 1;
+            else
+                left = pivotIndex + 1;
+        }
+    }
+
+    // Use median-of-three strategy to choose pivot
+    private int medianOf3(int[] nums, int left, int right) {
+        int mid = left + (right - left) / 2;
+        if (nums[right] > nums[left])
+            swap(nums, left, right);
+        if (nums[right] > nums[mid])
+            swap(nums, right, mid);
+        if (nums[mid] > nums[left])
+            swap(nums, left, mid);
+        return mid;
+    }
+
+    private int partition(int[] nums, int left, int right, int pivotIndex) {
+        int pivotValue = nums[pivotIndex];
+        swap(nums, pivotIndex, right);
+        int index = left;
+        for (int i = left; i < right; ++i) {
+            if (nums[i] > pivotValue) {
+                swap(nums, index, i);
+                ++index;
             }
         }
-        swap(a, i, hi);
-
-        // count the nums that are <= pivot from lo
-        int m = i - lo + 1;
-
-        // pivot is the one!
-        if (m == k)
-            return i;
-        // pivot is too big, so it must be on the left
-        else if (m > k)
-            return quickSelect(a, lo, i - 1, k);
-        // pivot is too small, so it must be on the right
-        else
-            return quickSelect(a, i + 1, hi, k - m);
+        swap(nums, right, index);
+        return index;
     }
 
-    void swap(int[] a, int i, int j) {
-        int tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
+    private void swap(int[] a, int x, int y) {
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
     }
 
 }
