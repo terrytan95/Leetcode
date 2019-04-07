5965f2a0d18f574198e92dfa24498e6bc0b9f8e2
Terry Tan <tmy1995hflc@gmail.com>
2019-04-07 03:16:21 -0500

    198,213

---
+++ 213.house-robber-ii.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=213 lang=java
+ *
+ * [213] House Robber II
+ *
+ * https://leetcode.com/problems/house-robber-ii/description/
+ *
+ * algorithms
+ * Medium (35.15%)
+ * Total Accepted:    110.3K
+ * Total Submissions: 313.8K
+ * Testcase Example:  '[2,3,2]'
+ *
+ * You are a professional robber planning to rob houses along a street. Each
+ * house has a certain amount of money stashed. All houses at this place are
+ * arranged in a circle. That means the first house is the neighbor of the last
+ * one. Meanwhile, adjacent houses have security system connected and it will
+ * automatically contact the police if two adjacent houses were broken into on
+ * the same night.
+ * 
+ * Given a list of non-negative integers representing the amount of money of
+ * each house, determine the maximum amount of money you can rob tonight
+ * without alerting the police.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,3,2]
+ * Output: 3
+ * Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money
+ * = 2),
+ * because they are adjacent houses.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money =
+ * 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ * 
+ */
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
+        return Math.max(Rob(nums, 0, n - 2), Rob(nums, 1, n - 1));
+    }
+
+    private int Rob(int[] nums, int start, int end) {
+        int prev = 0, cur = 0;
+        for (int i = start; i <= end; i++) {
+            int temp = cur;
+            cur = Math.max(nums[i] + prev, cur);
+            prev = temp;
+        }
+        return cur;
+    }
+}
