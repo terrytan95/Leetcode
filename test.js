15bc00abaa3a2dee23db281f37891cdfbd46feaf
Terry Tan <tmy1995hflc@gmail.com>
2019-04-25 04:59:50 -0500

    add mocha

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
+var expect = require("chai").expect;
+var describe = require("mocha").describe;
 var temp = 3.6;
 var test = function() {
-	return [-1, -1];
+	return 1;
 };
-console.log(5 / 2);
+
+describe("加法函数的测试", function() {
+	it("1 加 1 应该等于 2", function() {
+		expect(test()).to.be.equal(1);
+	});
+});
