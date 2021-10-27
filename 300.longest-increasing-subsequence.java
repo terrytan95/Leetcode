d83fab716e66e2a0b95d6514f35755d49bf86fee
Mingyao Tan <mtan@ibm.com>
2021-10-27 03:11:46 -0700

    287, 300, 334

---
+++ 300.longest-increasing-subsequence.java
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode id=300 lang=java
+ *
+ * [300] Longest Increasing Subsequence
+ *
+ * https://leetcode.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (47.22%)
+ * Likes:    9301
+ * Dislikes: 190
+ * Total Accepted:    682.5K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * Given an integer array nums, return the length of the longest strictly
+ * increasing subsequence.
+ *
+ * A subsequence is a sequence that can be derived from an array by deleting
+ * some or no elements without changing the order of the remaining elements.
+ * For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore
+ * the length is 4.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [0,1,0,3,2,3]
+ * Output: 4
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: nums = [7,7,7,7,7,7,7]
+ * Output: 1
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= nums.length <= 2500
+ * -10^4 <= nums[i] <= 10^4
+ *
+ *
+ *
+ * Follow up: Can you come up with an algorithm that runs in O(n log(n)) time
+ * complexity?
+ *
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        ArrayList<Integer> sub = new ArrayList<>();
+        sub.add(nums[0]);
+
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            if (num > sub.get(sub.size() - 1)) {
+                sub.add(num);
+            } else {
+                int j = binarySearch(sub, num);
+                sub.set(j, num);
+            }
+        }
+        return sub.size();
+    }
+    private int binarySearch(ArrayList<Integer> sub, int num) {
+        int left = 0;
+        int right = sub.size() - 1;
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if (sub.get(mid) == num) {
+                return mid;
+            }
+            if (sub.get(mid) < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left; //return the smallest number that's bigger than num
+    }
+}
+// @lc code=end
+
